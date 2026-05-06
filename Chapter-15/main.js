863c45759c4bb66fac21d2b3dea3cd9df72c14cd
DevMan825-lang <gouravtalukdar537@gmail.com>
2026-05-06 12:20:12 +0530

    Add files via upload

---
+++ Chapter-15/main.js
@@ -0,0 +1,76 @@
+// Search Methods of string in JavaScript : 
+// // 1. indexOf() : 
+// var myStr = "Please locate where 'locate' is occurs !"; 
+// console.log(myStr.indexOf("locate")); 
+
+
+// In the indexOf() method -1 returns in string 
+// if the value is not in the string.
+// var myStr = "Please locate where 'locate' is occurs !"; 
+// console.log(myStr.indexOf("John")); 
+
+
+// var myStr = "Please locate where 'locate' is occurs !"; 
+// console.log(myStr.indexOf("locate", 3)); 
+
+// 2. lastIndexOf() : 
+// var myStr = "Please locate where 'locate' is occurs !"; 
+// console.log(myStr.lastIndexOf("locate")); 
+
+// In the lastIndexOf() method -1 returns in string 
+// if the value is not in the string.
+// var myStr = "Please locate where 'locate' is occurs !"; 
+// console.log(myStr.lastIndexOf("John")); 
+
+// var myStr = "Please locate where 'locate' is occurs !"; 
+// console.log(myStr.lastIndexOf("locate", 25)); 
+
+// // 3. search() : 
+// var myStr = "Please locate where 'locate' is occurs !"; 
+// console.log(myStr.search("locate")); 
+
+// // 4. match() : 
+// var myStr = "Please locate where 'locate' is occurs !"; 
+// console.log(myStr.match("locate"));
+
+// 4. matchAll() : 
+// var myStr = "Please locate where 'locate' is occurs!"; 
+// var result = myStr.matchAll("locate"); 
+// console.log(Array.from(result));
+
+// 
+// 5. includes() : 
+    // var myStr = "This is JavaScript World !"; 
+    // console.log(myStr.includes("World"));
+    // console.log(myStr.includes("John")); 
+
+// 6. startsWith() : 
+    // var myStr = "Hello World, This is our universe !"; 
+    // console.log(myStr.startsWith("Hello")); 
+    // console.log(myStr.startsWith("This")); 
+       
+    // var myStr = "Hello World, This is our universe !"; 
+    // console.log(myStr.startsWith("World", 6)); 
+
+// 7. endsWith() : 
+    // var myStr = "Hello World, This is our universe"; 
+    // console.log(myStr.endsWith("universe")); 
+    // console.log(myStr.endsWith("World"));
+
+    // var myStr = "Hello World, This is our universe"; 
+    // console.log(myStr.endsWith("World", 11));
+
+    
+// 8. toString() : 
+    //  var myStr = "Hello World"; 
+    //  var result = myStr.toString(); 
+    //  console.log(typeof result); 
+
+    // var myNum = parseInt(100); 
+    // myNum = myNum.toString(); 
+    // console.log(typeof myNum); 
+
+
+
+
+
