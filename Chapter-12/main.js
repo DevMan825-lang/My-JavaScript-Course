122177ce6912a5d22876137cee5bc87875cba5e9
DevMan825-lang <gouravtalukdar537@gmail.com>
2026-05-06 12:13:04 +0530

    Add files via upload

---
+++ Chapter-12/main.js
@@ -0,0 +1,50 @@
+// querySelectors 
+// var x = document.getElementById("demo").innerHTML = "Hello World";
+// console.log(x); 
+
+// querySelector return the 1st element that matches a CSS selector. 
+// var x = document.querySelector("p"); 
+// console.log(x); 
+// document.querySelector("p").style.backgroundColor = "red"; 
+
+
+// Selects class name with querySelector 
+// var x = document.querySelector("p.example"); 
+// console.log(x); 
+
+// var x = document.querySelector(".example"); 
+// console.log(x); 
+
+// Selects Id name with querySelector 
+// var x = document.querySelector("#demo"); 
+// console.log(x); 
+// x.style.backgroundColor = "red"; 
+
+
+// Target Child Element in JavaScript...... 
+// function change(){
+//     var container = document.querySelector("div > p");
+//     console.log(container); 
+//     container.style.backgroundColor = "red"; 
+//     container.style.color = "#fff";  
+// }
+
+
+// Targeting attribute using the querySelector() : 
+// To target HTML attributes in JavaScript first 
+// write down your HTML tag then under the ([]) 
+// square barcket give the attribute name of 
+// the HTML tag.......... 
+// Syntax : tagName[attributeName]; 
+            // Ex: a[href]. 
+// var link_1 = document.querySelector("a[href]"); 
+// link_1.style.backgroundColor = "red"; 
+// var link_2 = document.querySelector("a[target]"); 
+// link_2.style.backgroundColor = "yellow"; 
+// var link_3 = document.querySelector("a[hreflang]"); 
+// link_3.style.backgroundColor = "green"; 
+// var link_4 = document.querySelector("a[rel]"); 
+// link_4.style.backgroundColor = "orange"; 
+
+
+// document.querySelector("h3, h4").style.backgroundColor = "red"; 
