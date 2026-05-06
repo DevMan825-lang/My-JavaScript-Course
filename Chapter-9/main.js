09410b1f8cab23e5560e17dd3fede929cbdd2d22
DevMan825-lang <gouravtalukdar537@gmail.com>
2026-05-06 12:02:07 +0530

    Add files via upload

---
+++ Chapter-9/main.js
@@ -0,0 +1,102 @@
+// Conditional Statements : 
+// IF   // True condition
+// Else IF // To create multiple conditions 
+// Else // False condition 
+// var age = 15; 
+// if(age >= 18){
+//     console.log("You are adult"); 
+// }else{
+//     console.log("You are minor"); 
+// }
+
+// var mark = 65; 
+// if(mark >= 95 && mark <= 100){
+//     console.log("Stand"); 
+// }
+// else if(mark >= 85 && mark <= 75){
+//     console.log("Star"); 
+// }
+// else if(mark >= 60 && mark <= 75){
+//     console.log("First"); 
+// }
+// else if(mark >= 45 && mark <= 60){
+//     console.log("Second"); 
+// }
+// else if(mark >= 35 && mark <= 45){
+//     console.log("Pass"); 
+// }
+// else{
+//     console.log("Fail"); 
+// }
+
+// var readline = require("readline"); 
+// var rl = readline.createInterface({
+//         input:process.stdin, 
+//         output:process.stdout 
+// });
+// rl.question("Enter the day here : ", function(day){
+//         if(day == "monday"){
+//             console.log("Today is monday go for work"); 
+//         } else if(day == "tuesday"){
+//             console.log("Today is tuesday go for work"); 
+//         }else if(day == "wednesday"){
+//             console.log("Today is wednesday go for work"); 
+//         } else if(day == "thursday"){
+//             console.log("Today is thursday go for work"); 
+//         } else if(day == "friday"){
+//             console.log("Today is thursday go to church"); 
+//         } else if(day == "saturday"){
+//             console.log("Today is thursday watch movie at home"); 
+//         }  else if(day == "Sunday"){
+//             console.log("Today is sunday rest"); 
+//         } else {
+//             console.log("Invalid day"); 
+//         }
+// }); 
+
+// switch case statement : 
+// Syntax : 
+            // switch(varName){
+            //     case 1 : 
+            //     // case 1 Condition 
+            //     break; 
+            //     case 2 : 
+            //     // Case 2 Condition 
+            //     break; 
+
+            //     case 3 : 
+            //     // Case 3 Condition 
+            //     break; 
+
+            //     default : 
+
+        // }
+
+
+    // var day = 8; 
+    //         switch(day){
+    //             case 1 : 
+    //             console.log("Today is Monday"); 
+    //             break; 
+    //             case 2 : 
+    //             console.log("Today is Tuesday"); 
+    //             break; 
+    //             case 3 : 
+    //             console.log("Today is Wednesday"); 
+    //             break; 
+    //             case 4 : 
+    //             console.log("Today is Thursday"); 
+    //             break; 
+    //             case 5 : 
+    //             console.log("Today is Friday"); 
+    //             break; 
+    //             case 6 : 
+    //             console.log("Today is Saturday"); 
+    //             break; 
+    //             case 7 :
+    //             console.log("Today is sunday"); 
+    //             default : 
+    //             console.log("Invalid day"); 
+    //         }
+
+    
