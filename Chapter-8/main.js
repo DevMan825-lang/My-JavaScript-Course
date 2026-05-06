287e04bb8dee01372abb2216efbb61d8060bf927
DevMan825-lang <gouravtalukdar537@gmail.com>
2026-05-06 12:01:01 +0530

    Add files via upload

---
+++ Chapter-8/main.js
@@ -0,0 +1,120 @@
+// Operators 
+// 5 + 5 
+// = 10 
+// Operator is used for doing operations in numbers or we can
+// create logical conditions with this operators 
+
+// 1. Arithmetic Opeators....... 
+// 2. Assignment Operators....... 
+// 3. Comparison Operators.......
+// 4. Logical Operators..........
+// 5. Ternary Operator...........
+
+// 1. Arithmetic Opeators....... 
+// Addition(+)
+// Subtraction(-)
+// Multiplication(*)
+// Division(/)
+// ** 
+// % (Modulus)
+// ++
+// -- 
+
+// var num_1 = 20; 
+// var num_2 = 10; 
+// console.log(num_1 + num_2); 
+// console.log(num_1 - num_2); 
+// console.log(num_1 * num_2); 
+// console.log(num_1 / num_2); 
+
+// var num = 4; 
+// console.log(num ** 4); 2 * 2 * 2 * 2 = 16
+// console.log(num % 3); 
+// num++; 
+// console.log(num);
+// num--;  
+// console.log(num); 
+// num-- 
+// console.log(num); 
+// num--
+// console.log(num); 
+// num-- 
+// console.log(num); 
+// num++
+// console.log(num); 
+
+
+
+// 2. Assignment Opeators.......
+// Equal To(=) 
+// Equal to Addition(+=)
+// Equal to Subtraction(-=)
+// Equal to Multiplication(*=)
+// Equal to  Division(/)
+// Equal to Exponention(**=) 
+// %= (Equal to Modulus)
+
+// num += 2; 
+// num = num + 2  
+// num -= 2; 
+// num *= 2; 
+// num /= 2; 
+// num **= 2; 
+// num %= 2; 
+// console.log(num);
+
+// 3. Comparison Operators : 
+// Equal To (==) == When the both values are equal
+// Equal To Type (===) = When the both values and types are equal
+// Not Equal To (!=) = When the both values are not equal
+// Not Equal To Type (!==) = When the both values and there type are not equal
+// Greater Than (>) = When the left side value is greater than the right side
+// Less Than (<) = When the right side value is greater than the left side
+// Greater Than Equal (>=) = When the left side value is greater than the right side or sometimes it equals
+// Less Than Equal (==) When the right side value is greater than the left side or sometimes it equals 
+
+// console.log(5 == 5); 
+// console.log(5 == 10); 
+// console.log(5 != 10); 
+// console.log(5 != 5); 
+// console.log(10 > 4); 
+// console.log(5 < 10);  
+// console.log(10 > 10); // false 
+// console.log(10 >= 10); // true 
+// console.log(5 <= 5); // true 
+
+
+// 4. Logical Operators : 
+// AND (&&)
+// OR (||)
+// NOT (!)
+
+// AND 
+// console.log(5 == 5 && 5 > 3); 
+// console.log(5 == 5 && 5 < 3); 
+
+// OR 
+// console.log(5 == 5 || 5 < 3); 
+
+// NOT 
+// console.log(!(5 == 5 && 5 > 3)); // False
+// console.log(!(5 == 5 && 5 < 3)); // True 
+// console.log(!(5 == 5 || 5 < 3)); // False  
+
+// Ternary Opreator : 
+// var age = 12; 
+// var check = (age >= 18) ? "Adult" : "Minor"; 
+// console.log(check); 
+
+// Grade System : 
+// var mark = 32; 
+// var grade = (mark >= 85) ? 'A+' : 
+//             (mark >= 75) ? 'A' : 
+//             (mark >= 60) ? 'B+' : 
+//             (mark >= 50) ? 'C' : 
+//             (mark >= 45) ? 'D' : 
+//             (mark >= 35) ? 'E' : "F"; 
+// console.log("Grade : " + grade); 
+            
+
+
