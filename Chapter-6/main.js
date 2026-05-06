a4cb1e739097cdb9d614a7bfa047f40365e0e0ca
DevMan825-lang <gouravtalukdar537@gmail.com>
2026-05-06 11:55:20 +0530

    Add files via upload

---
+++ Chapter-6/main.js
@@ -0,0 +1,32 @@
+// var myString = "42";
+// var myNumber = parseInt(myString); 
+// console.log(myNumber); 
+// console.log(typeof myNumber); 
+
+// var myString_1 = "10";
+// var myString_2 = "10"; 
+// var myNumber_1 = parseInt(myString_1); 
+// var myNumber_2 = parseInt(myString_2); 
+// var result = myNumber_1 + myNumber_2; 
+// console.log(result); 
+
+// var myString_1 = "10.5";
+// var myString_2 = "10.5"; 
+// var myNumber_1 = parseFloat(myString_1); 
+// var myNumber_2 = parseFloat(myString_2); 
+// var result = myNumber_1 + myNumber_2; 
+// console.log(result); 
+
+// console.log(parseInt(10+10));
+
+// console.log(parseInt(&123)); 
+// console.log(parseInt(-123)); 
+// console.log(parseInt("xyz"));
+
+
+// NaN (Not a Number)
+// console.log(parseInt("xyz"));
+// console.log(parseInt("-123")); 
+
+// console.log(NaN == NaN); 
+// Two NaN and NaN cannot be equal......  
