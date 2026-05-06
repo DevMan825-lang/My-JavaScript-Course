e7e7fd6c70603396932a770c44fd7a5a762b392c
DevMan825-lang <gouravtalukdar537@gmail.com>
2026-05-06 11:56:21 +0530

    Add files via upload

---
+++ Chapter-7/Main.js
@@ -0,0 +1,9 @@
+// Type Coercion : 
+// Type Coercion can be return unexpected values..... 
+// 1) Explicit,  2) Implicit
+
+// var result = "5" * 5; // Type Coercion  
+// console.log(result); 
+
+//  var result = "5" + 5; // Concatenattion   
+//  console.log(result); 
