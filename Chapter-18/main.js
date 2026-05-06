c9b42ee3ca12ad06afb43cbb89a29e45238ad95c
DevMan825-lang <gouravtalukdar537@gmail.com>
2026-05-06 12:24:17 +0530

    Add files via upload

---
+++ Chapter-18/main.js
@@ -0,0 +1,42 @@
+// let, var, const. 
+// var 
+// let and const 
+// let and const introduce in ECMAScript 2015. 
+
+// var x = 10;
+// var x = 20; 
+// console.log(x); 
+
+// let x = 10; 
+// let x = 20; 
+// console.log(x); 
+
+// let x = 10; 
+// x = 20; 
+// console.log(x); 
+
+// {
+//     let x = 10; 
+//     let x = 20; 
+//    console.log(x); 
+
+// }
+
+// var x = 10; 
+// x = 20; 
+// console.log(x); 
+
+// let x = 10; 
+// x = 20; 
+// console.log(x); 
+
+// const x = 10; 
+// x = 20; 
+// console.log(x)
+// ; 
+
+// const x = 10; 
+// const x = 20; 
+// console.log(x); 
+
+
